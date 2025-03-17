2e7cb43b4b69ff636bb60c331a9ea8c56e8e3b9d
hoang261105 <147359595+hoang261105@users.noreply.github.com>
2025-03-17 09:19:09 +0700

    Update Bai7

--- src/Bai7/Main.java
+++ src/Bai7/Main.java
@@ -9,16 +9,8 @@ public class Main {
         vehicles[2] = new Bus();
 
         for (Vehicle v : vehicles){
-            if(v instanceof Car){
-                System.out.println(v.move());
-                System.out.println(v.sound());
-            }else if(v instanceof Bike){
-                System.out.println(v.move());
-                System.out.println(v.sound());
-            }else{
-                System.out.println(v.move());
-                System.out.println(v.sound());
-            }
+            System.out.println(v.move());
+            System.out.println(v.sound());
         }
     }
 }
